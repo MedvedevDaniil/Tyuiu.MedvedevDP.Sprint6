71be2e921b914f841fb533405ffba78b7a0a8d6b
danii <danii@DANIIL>
2025-12-13 23:32:35 +0500

    sdf

---
+++ Tyuiu.MedvedevDP.Sprint6.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.MedvedevDP.Sprint6.Task1.V18.Lib
+{
+    public class DataService : ISprint6Task1V18
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double y;
+            int len = (stopValue - startValue) + 1;
+            double[] res = new double[len];
+            int cnt = 0;
+            for (int i = startValue; i < stopValue + 1; i++)
+            {
+                y = (2 * Math.Sin(i)) / (3 * i + 1.2) + Math.Cos(i) - 7 * i * 2;
+                y = Math.Round(y, 2);
+                res[cnt] = y;
+                cnt++;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint6.Task1.V18/FormMain.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Forms.VisualStyles;
+using Tyuiu.MedvedevDP.Sprint6.Task1.V18.Lib;
+namespace Tyuiu.MedvedevDP.Sprint6.Task1.V18
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int start = Convert.ToInt32(textBoxStart.Text);
+                int end = Convert.ToInt32(textBoxEnd.Text);
+
+                string strLine;
+
+                int len = ds.GetMassFunction(start, end).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+                valueArray = ds.GetMassFunction(start, end);
+                textBoxResult.Text = "";
+                textBoxResult.Text = ("+----------+----------+" + Environment.NewLine);
+                textBoxResult.Text = ("|    X     |   f(x)   |" + Environment.NewLine);
+                textBoxResult.Text = ("+----------+----------+" + Environment.NewLine);
+
+                for (int i = 0; i < len - 1; i++)
+                {
+                    strLine = String.Format("| {0,5:d}    |  {1, 5:f2}   | ", start, valueArray[i]);
+                    textBoxResult.AppendText(strLine + Environment.NewLine);
+                    start++;
+                }
+
+                textBoxResult.AppendText("+----------+----------+" + Environment.NewLine);
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Ââåäåíû íåâåðíûå äàííûå", "Îøèáêà", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Òàñê 1 âûïîëíèë ñòóäåíò ãðóïïû ÏÈÍá-25-1 Êàæàõìåòîâ Àìèð Åðáîëîâè÷", "Ñîîáùåíèå", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
