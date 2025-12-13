fc7ed1a80d4c420497eedc9849c0cc0490026ca5
danii <danii@DANIIL>
2025-12-13 23:03:32 +0500

    Add project files.

---
+++ Tyuiu.MedvedevDP.Sprint6.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MedvedevDP.Sprint6.Task0.V16.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint6.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double res = ds.Calculate(x);
+            double wait = 3.111;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint6.Task0.V16/FormMain.cs
@@ -0,0 +1,35 @@
+﻿
+using Tyuiu.MedvedevDP.Sprint6.Task0.V16.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint6.Task0.V16
+{
+    public partial class FormMain : Form
+    {
+        private DataService ds = new DataService();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = 3;
+                double result = ds.Calculate(x);
+                textBoxResult.Text = $"Результат x = {x}: {result:F3}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ответ: " + ex.Message);
+            }
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ТЕСТ",
+                "ТЕст", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
